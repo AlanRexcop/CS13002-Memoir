6e77c787247f357f65389d1943748ebc57cad489
Nguyen Gia Huy <giahuydev26@gmail.com>
2025-08-13 09:32:23 +0700

    feat: check weak password

--- lib/screens/change_password_screen.dart
+++ lib/screens/change_password_screen.dart
@@ -99,6 +99,10 @@ class ChangePasswordScreen extends ConsumerWidget {
                   if (value == null || value.length < 6) {
                     return 'Password must be at least 6 characters long';
                   }
+                  if (!value.contains(RegExp(r'[A-Z]'))) return 'Password must contain an uppercase letter.';
+                  if (!value.contains(RegExp(r'[a-z]'))) return 'Password must contain a lowercase letter.';
+                  if (!value.contains(RegExp(r'[0-9]'))) return 'Password must contain a number.';
+                  if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) return 'Password must contain a special character.';
                   return null;
                 },
               ),
